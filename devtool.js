2f15d0ea701a317cde08a3170ef967a5ab7136c1
Bill Huang <bill.huang2001@gmail.com>
2021-12-21 20:46:18 +0800

    re-init

--- devtool.js
+++
@@ -1,9 +0,0 @@
-//console.log('Hello from -> Devtool');
-chrome.devtools.panels.create(
-	"CS315-Checker",
-	"icon.gif",
-	"content.html",
-	function(panel) { 
-		console.log("Content is loaded to panel"); 
-	}
-);
