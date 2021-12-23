0aab94064cc23396f6cf907ffe1df66ea94e71db
Daniel Xu <11912921@mail.sustech.edu.cn>
2021-12-23 20:02:39 +0800

    popup ui

--- popups/index.js
+++ popups/index.js
@@ -1,24 +1,22 @@
-function listenForClicks() {
-    document.addEventListener("click", (e) => {
-        if (e.target.classList.contains("reset")) {
-            refresh()
-        }
+$(document).ready(function () {
+    $(".reset").click(() => {
+        browser.storage.local.get("url2hash").then((result) => {
+            reset(result.url2hash);
+        }).catch((err) => { console.log(err); });
     });
-}
+})
+
 
 function reset(url2hash) {
-    let content = []
-    for(const [key, value] of Object.entries(url2hash)) {
-        content.push(`<div> ${key} : ${value} </div>`)
-    }
-    document.getElementById("popup-content").innerHTML = content.join("\n")
+    let $table = $("#site-hash-table")
+    $table.children("tbody").remove()
+    Object.entries(url2hash).forEach((value, index) => {
+        let str1 = "<tbody><tr><td>" + index + "</td>"
+        let str2 = "<td>" + value[0] + "</td>"
+        let str3 = "<td>" + value[1] + "</td>"
+        let str4 = "</tr></tbody>"
+        $table.append(str1 + str2 + str3 + str4)
+        // console.log(value)
+    })
+    // document.getElementById("site-hash-table").innerHTML = content.join("\n")
 }
-
-function refresh() {
-    browser.storage.local.get("url2hash").then((result) => {
-        reset(result.url2hash);
-    }).catch((err) => { console.log(err); });
-}
-
-
-listenForClicks()
