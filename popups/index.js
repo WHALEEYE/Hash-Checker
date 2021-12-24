fdb599d0c0169d043c60c547eca1ef3cd409c4cb
Daniel Xu <11912921@mail.sustech.edu.cn>
2021-12-24 10:27:21 +0800

    index.js

--- popups/index.js
+++ popups/index.js
@@ -9,22 +9,12 @@ var stored_url2hash = {}
 function reset(url2hash) {
 
     stored_url2hash = url2hash
-
-    var domain
-
-
     function logTabs(tabs) {
-        for (let tab of tabs) {
-            const regex = /(?:[\w-]+\.)+[\w-]+/;
-            domain = regex.exec(tab.url);
-            if (domain !== null) {
-                domain = domain[0]
-            }
-            $("#domain-selector").children("[selected]").text(domain)
-        }
+        let current_url = tabs[0].url
+        $("#domain-selector").children("[selected]").text(current_url)
 
         Object.entries(url2hash).forEach((value) => {
-            if (domain === value[0]) {
+            if (current_url === value[0]) {
                 refresh_table(value[1])
             }
         })
@@ -41,7 +31,7 @@ function reset(url2hash) {
 function refresh_table(hashtable) {
     let $table = $("#site-hash-table")
     $table.children("tbody").remove()
-    Object.entries(url2hash).forEach((value, index) => {
+    Object.entries(hashtable).forEach((value, index) => {
         // siteUrl is key
         // content: {
         //     url1: hash,
@@ -50,13 +40,13 @@ function refresh_table(hashtable) {
         //     title: String,
         //     iconUrl: String,
         // }
-        const [siteUrl, content] = value;
+        // const [siteUrl, content] = value;
         let str1 = "<tbody><tr><td>" + index + "</td>";
-        let icon = `<td> <img src=${content.iconUrl}> </td>`;
-        let str2 = "<td>" + content.title + "</td>"
+        // let icon = `<td> <img src=${content.iconUrl}> </td>`;
+        let str2 = "<td>" + value.title + "</td>"
         let str3 = "<td>" + value[1] + "</td>"
         let str4 = "</tr></tbody>"
         $table.append(str1 + icon + str2 + str3 + str4)
-        // console.log(value)
+        console.log(value)
     })
 }
